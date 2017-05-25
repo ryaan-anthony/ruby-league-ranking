995117b217fbec63e05f5ebe97cb0b92d15a1634
Tulino, Ryan <rtulino@radial.com>
2017-05-25 19:36:56 -0400

    use POINTS constant and shorten length of param name

--- app/services/calculate_game_results.rb
+++ app/services/calculate_game_results.rb
@@ -11,12 +11,12 @@ module LeagueRankings
       @league = league
     end
 
-    def calculate_result(game_result)
-      home_team = @league.team_by_name(game_result.home_name)
-      away_team = @league.team_by_name(game_result.away_name)
-      away_team.award(POINTS_WIN) if game_result.away_won?
-      home_team.award(POINTS_WIN) if game_result.home_won?
-      away_team.award(POINTS_TIE) & home_team.award(1) if game_result.tie?
+    def calculate_result(result)
+      home_team = @league.team_by_name(result.home_name)
+      away_team = @league.team_by_name(result.away_name)
+      away_team.award(POINTS_WIN) if result.away_won?
+      home_team.award(POINTS_WIN) if result.home_won?
+      away_team.award(POINTS_TIE) & home_team.award(POINTS_TIE) if result.tie?
     end
 
     def calculate
